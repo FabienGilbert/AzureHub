333ab2e785295dff7d744293711d9ced79a6252f
Fabien Gilbert <fgilbert@microsoft.com>
2021-05-03 14:40:10 -0400

    added public dns zone and firewall off

--- automation/dsc/configurations/newActiveDirectoryForest.ps1
+++ automation/dsc/configurations/newActiveDirectoryForest.ps1
@@ -9,19 +9,26 @@ configuration newActiveDirectoryForest
         [string] $DnsForwarder,
  
         [Parameter(Mandatory)]
-        [string] $automationConnectionName
+        [string] $automationConnectionName,
+ 
+        [Parameter()]
+        [string] $windowsFirewallEnabled = 'false'
     )
  
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DSCResource -ModuleName StorageDsc    
     Import-DSCResource -ModuleName xActiveDirectory
     Import-DSCResource -ModuleName xDnsServer
+    Import-DscResource -ModuleName NetworkingDsc 
+    Import-DSCResource -ModuleName ComputerManagementDsc  
  
+    # Connect to Azure tenant
     $automationConnection = Get-AutomationConnection -Name $automationConnectionName
     Connect-AzAccount -Tenant $automationConnection.TenantID -ApplicationId $automationConnection.ApplicationID -CertificateThumbprint $automationConnection.CertificateThumbprint
   
     Node $AllNodes.NodeName
     {
+        # Wait for data disk
         WaitForDisk Disk2
         {
             DiskId           = 2
@@ -29,6 +36,7 @@ configuration newActiveDirectoryForest
             RetryCount       = 60
         } 
 
+        # Initialize and format data disk
         Disk FVolume
         {
             DiskId      = 2
@@ -37,6 +45,7 @@ configuration newActiveDirectoryForest
             DependsOn   = '[WaitForDisk]Disk2'
         }
         
+        # Install Windows Features
         WindowsFeature DNS 
         { 
             Ensure = "Present" 
@@ -72,11 +81,37 @@ configuration newActiveDirectoryForest
 			DependsOn = '[WindowsFeature]RSATADTools'
         }
         
+        # Change time zone
+        TimeZone EasternTime
+        {
+            IsSingleInstance = 'Yes'
+            TimeZone         = 'Eastern Standard Time'
+        }
+
+        # Configure Windows Firewall Profiles
+        FirewallProfile FirewallProfileDomain
+        {
+            Name = 'Domain'
+            Enabled = $windowsFirewallEnabled
+        }
+        FirewallProfile FirewallProfilePrivate
+        {
+            Name = 'Private'
+            Enabled = $windowsFirewallEnabled
+        }
+        FirewallProfile FirewallProfilePublic
+        {
+            Name = 'Public'
+            Enabled = $windowsFirewallEnabled
+        }
+
+        # Get secrets from Key vault
         $daSecret = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $Node.daUsername
         $daCredentials = New-Object System.Management.Automation.PSCredential (($Node.DomainName + "\" + $Node.daUsername), $daSecret.SecretValue)
         $djSecret = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $Node.djUsername
         $djCredentials = New-Object System.Management.Automation.PSCredential (($Node.DomainName + "\" + $Node.djUsername), $djSecret.SecretValue)
        
+        # Create AD Forest
         xADDomain FirstDS 
         {
             DomainName = $Node.DomainName
@@ -87,6 +122,8 @@ configuration newActiveDirectoryForest
             SysvolPath = "F:\SYSVOL"
 			DependsOn = '[WindowsFeature]GPMC'
         }
+
+        # Configure DNS Forwarders
         xDnsServerForwarder 'SetForwarders'
         {
             IsSingleInstance = 'Yes'
@@ -94,6 +131,8 @@ configuration newActiveDirectoryForest
             UseRootHint      = $false
 			DependsOn = '[xADDomain]FirstDS'
         }
+
+        # Create Domain join user
         xADUser 'domainJoinUser'
         {
             Ensure = 'Present'
@@ -104,6 +143,8 @@ configuration newActiveDirectoryForest
             Path = ("CN=Users,DC=" + $Node.DomainName.replace(".",",DC="))
 			DependsOn = '[xDnsServerForwarder]SetForwarders'
         } 
+
+        # Create Servers OU
         xADOrganizationalUnit 'ServersOU'
         {
             Name = "Servers"
@@ -114,6 +155,36 @@ configuration newActiveDirectoryForest
 			DependsOn = '[xADDomain]FirstDS'
         } 
         
+        # Create internal public DNS zone
+        xDnsServerADZone 'AddForwardADZone'
+        {
+            Name             = $Node.PublicDomainName
+            DynamicUpdate    = 'Secure'
+            ReplicationScope = 'Forest'
+            Ensure           = 'Present'
+			DependsOn        = '[xDnsServerForwarder]SetForwarders'
+        }
+
+        # Create A DNS Host records
+        xDnsRecord 'AddStsArecord'
+        {
+            Name   = $Node.StsRecordName
+            Target = $Node.StsRecordAddress
+            Zone   = $Node.PublicDomainName
+            Type   = 'ARecord'
+            Ensure = 'Present'
+			DependsOn   = '[xDnsServerADZone]AddForwardADZone'
+        }
+        xDnsRecord 'AddCaArecord'
+        {
+            Name   = $Node.CaRecordName
+            Target = $Node.CaRecordAddress
+            Zone   = $Node.PublicDomainName
+            Type   = 'ARecord'
+            Ensure = 'Present'
+			DependsOn   = '[xDnsServerADZone]AddForwardADZone'
+        }
+
         LocalConfigurationManager 
         {
             RebootNodeIfNeeded = $True
