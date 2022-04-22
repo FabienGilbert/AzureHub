41f4450bfd08233c9a9797abcb3aa1cc84fa6fed
Fabien Gilbert <fgilbert@microsoft.com>
2022-04-22 12:34:06 -0400

    support for inline selection

--- misc/setSubscription.ps1
+++ misc/setSubscription.ps1
@@ -1,1 +1,15 @@
-Get-AzContext -ListAvailable | Sort-Object -Property {$_.Account.id}, {$_.Subscription.Name} |  Out-GridView -PassThru | Set-AzContext
+$allContexts = Get-AzContext -ListAvailable
+$sortedAllContexts = $allContexts | Sort-Object -Property {$_.Account.id}, {$_.Subscription.Name}
+$indexedAllContexts = $sortedAllContexts | Select-Object -Property @{l="Index";e={[array]::IndexOf($sortedAllContexts,$_)}},@{l="Login";e={$_.Account.id}}, @{l="Subscription";e={$_.Subscription.Name}}
+$indexedAllContexts | Format-Table -AutoSize
+$selectId = Read-Host -Prompt "Subscription # to switch context to?"
+if($selectId -and ($indexedAllContexts | Select-Object -ExpandProperty Index) -contains $selectId){
+    $selectSub = $sortedAllContexts[$selectId]
+    if((Get-AzContext).Subscription.Id -ne $selectSub.Subscription.Id)
+    {
+        $selectSub | Set-AzContext
+    }
+}
+else{
+    exit
+}
